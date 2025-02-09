4202a95139467c2e49528d9905ab2b8468091901
Evboys <evanboy@hotmail.fr>
2025-02-09 12:47:14 +0100

    Ajout du GitIgnore, MAJ du contenu

---
+++ index.js
@@ -0,0 +1,12 @@
+// -- utilisation du serveur web Express
+const express = require("express");
+const app = express();
+// -- connexion à la BD si besoin
+// -- utilisation du router pour la gestion de sportifs
+const { routerBooks } = require("./app/router/routerBooks");
+app.use(routerBooks);
+// -- lancer le serveur pour qu'il écoute sur le port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+ console.log(`le serveur REST est lancé sur le port : ${PORT}`);
+});
